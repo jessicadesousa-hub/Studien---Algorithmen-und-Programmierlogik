d7ef74609145de72b302acdc996b171f04dc5679
Jessica <jessicadesousa054@gmail.com>
2026-09-16 16:11:53 +0200

    Delete Para directory

--- Para/COMBINACOES/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        for (int ersterZaehler = 1; ersterZaehler <= 3; ersterZaehler++) {
-            for (int zweiterZaehler = 1; zweiterZaehler <= 3; zweiterZaehler++) {
-                System.out.println(ersterZaehler + " " + zweiterZaehler);
-            }
-        }
-    }
-}
